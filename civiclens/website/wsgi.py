bacab65213ace2b0fbf5b2633b63fa0ff8256661
jchristensonCHI <114839369+jchristensonCHI@users.noreply.github.com>
2024-04-12 17:49:25 -0500

    noqa additions

--- civiclens/website/wsgi.py
+++ civiclens/website/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os # noqa
+import os # noqa: F401
 
 from django.core.wsgi import get_wsgi_application # noqa
 
