9c1760808d690f0b73f2f38ee93375a384f3a13f
Reza <rezarzky@users.noreply.github.com>
2024-04-12 15:19:24 -0500

    update

--- civiclens/data_engineering/db_init.sql
+++ civiclens/data_engineering/db_init.sql
@@ -1,20 +1,3 @@
--- Creating the Role table
-CREATE TABLE Role (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    description TEXT
-);
-
--- Creating the User table
-CREATE TABLE User (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    role_id INT,
-    FOREIGN KEY (role_id) REFERENCES Role(id)
-);
-
 -- Creating the Dockets table
 CREATE TABLE Dockets (
     id VARCHAR(255) PRIMARY KEY,
@@ -36,24 +19,35 @@ CREATE TABLE Documents (
     agencyId VARCHAR(100),
     commentEndDate DATE,
     postedDate DATE,
-    title TEXT,
+    docTitle TEXT,
     docketId VARCHAR(255) REFERENCES Dockets(id),
     subtype VARCHAR(255),
     commentStartDate DATE,
     openForComment BOOLEAN,
-    objectId VARCHAR(100)
+    objectId VARCHAR(100),
+    fullTextXmlUrl VARCHAR(255),
+    subAgy VARCHAR(255),
+    agencyType VARCHAR(100),
+    CFR VARCHAR(100),
+    RIN VARCHAR(100),
+    title VARCHAR(255),
+    summary TEXT,
+    dates VARCHAR(255),
+    furtherInformation TEXT,
+    supplementaryInformation TEXT,
+    fullText TEXT
 );
 
 -- Creating the Comments table
-CREATE TABLE Comments (
+CREATE TABLE PublicComments (
     id VARCHAR(255) PRIMARY KEY,
     objectId VARCHAR(255) NOT NULL,
     commentOn VARCHAR(255),
-    commentOnDocumentId TIMESTAMP REFERENCES Documents(id),
+    commentOnDocumentId VARCHAR(255) REFERENCES Documents(id),
     duplicateComments INT,
     stateProvinceRegion VARCHAR(100),
     subtype VARCHAR(100),
-    comment TEXT,
+    publicComment TEXT,
     firstName VARCHAR(255),
     lastName VARCHAR(255),
     address1 VARCHAR(200),
@@ -71,7 +65,7 @@ CREATE TABLE Comments (
     pageCount INT,
     postedDate DATE,
     receiveDate DATE,
-    title TEXT,
+    commentTitle TEXT,
     trackingNbr VARCHAR(255),
     withdrawn BOOLEAN,
     reasonWithdrawn VARCHAR(255),
@@ -83,19 +77,4 @@ CREATE TABLE Comments (
     submitterRepCityState VARCHAR(100)
 );
 
--- Creating the Summaries table
-CREATE TABLE Summaries (
-    id VARCHAR(255) PRIMARY KEY,
-    frDocNum VARCHAR(100) REFERENCES Documents(frDocNum),
-    fullTextXmlUrl VARCHAR(255),
-    subAgy VARCHAR(255),
-    agencyType VARCHAR(100),
-    CFR VARCHAR(100),
-    RIN VARCHAR(100),
-    title VARCHAR(255),
-    summary TEXT,
-    dates VARCHAR(255),
-    furtherInformation TEXT,
-    supplementaryInformation TEXT,
-    fullText TEXT
-);
+
