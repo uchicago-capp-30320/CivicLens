9fa5d5ee8c1aaf2904f45896a0a1235af7437b9c
Abe Burton <abeburton@Abes-MacBook-Pro.local>
2024-04-25 18:21:06 -0500

    update nlp with docstrings and type hinting

--- civiclens/nlp/comments.py
+++ civiclens/nlp/comments.py
@@ -15,6 +15,18 @@ def pull_data(
     database_host: str,
     database_port: str,
 ) -> pl.DataFrame:
+    """gets the largest group of doc comments currently in database for sample
+
+    Args:
+        database (str): db name
+        database_user (str): db user
+        database_password (str): db password
+        database_host (str): db host
+        database_port (str): db port
+
+    Returns:
+        pl.DataFrame: polars df of comment data
+    """ """"""
     try:
         connection = psycopg2.connect(
             database=database,
@@ -25,7 +37,6 @@ def pull_data(
         )
 
         cursor = connection.cursor()
-        # get largest group of doc comments currently in database for sample
         query = """
             SELECT id, comment
             FROM regulations_comment
@@ -55,32 +66,73 @@ def pull_data(
     return df
 
 
-def comment_similarity(df):
+def comment_similarity(df: pl.DataFrame) -> pl.DataFrame:
+    """Create df with comment mappings and their semantic similarity scores
+    according to the SBERT paraphrase mining mthid using the all-mpnet-base-v2
+    model from hugging face. A minimum similarity of .85 is used for inclusion
+    following the methodology of this CDO report:
+    https://www.cdo.gov/news/public-comment-analysis-pilot/
+
+    Args:
+        df (pl.DataFrame): comment data
+
+    Returns:
+        pl.DataFrame: df with pairs of comment indices and a cosine similarity
+    """
     model = SentenceTransformer("all-mpnet-base-v2")
     paraphrases = util.paraphrase_mining(
         model, df["comment"].to_list(), show_progress_bar=True
     )
-    # CDO heuristic is .85 as a threshold for considering comments similar
     df_paraphrases = pl.DataFrame(
         paraphrases, schema=["similarity", "idx1", "idx2"]
     ).filter(pl.col("similarity") >= 0.85)
+
     return df_paraphrases
 
 
-def build_graph(df):
+def build_graph(df: pl.DataFrame) -> nx.Graph:
+    """Builds a network graph with comments as nodes and their similarities as
+    weights
+
+    Args:
+        df (pl.DataFrame): df with pairs of comment indices and a cosine
+        similarity
+
+    Returns:
+        nx.Graph:network graph with comments as nodes and their similarities as
+        weights
+    """
     graph_data = df.to_dicts()
     G = nx.Graph()
-    # Add the edges along with their weights to the graph
     for edge in graph_data:
         G.add_edge(edge["idx1"], edge["idx2"], weight=edge["similarity"])
+
     return G
 
 
-def get_clusters(G):
+def get_clusters(G: nx.Graph) -> list[set[int]]:
+    """Defines clusters based on the Louvain Communities algorithm
+
+    Args:
+        G (nx.Graph): network graph with comments as nodes and their
+        similarities as weights
+
+    Returns:
+        list[set[int]]: sets are clusters of comment nodes
+    """
     return louvain_communities(G=G)
 
 
-def assign_clusters(df, clusters):
+def assign_clusters(df: pl.DataFrame, clusters: list[set[int]]) -> pl.DataFrame:
+    """Inserts cluster info into the polars df of data from the initial pull
+
+    Args:
+        df (pl.DataFrame): df from initial pull
+        clusters (list[set[int]]): clusters from Louvain Communities
+
+    Returns:
+        pl.DataFrame: updated df
+    """
     # create null column for clusters (this should exist in sql eventually)
     rows = df.shape[0]
     df = df.with_columns(pl.Series("cluster", [None] * rows).cast(pl.Utf8))
@@ -93,22 +145,51 @@ def assign_clusters(df, clusters):
             .otherwise(pl.col("cluster"))
             .alias("cluster")
         )
+
     return df
 
 
-def find_central_node(G, clusters):
+def find_central_node(G: nx.Graph, clusters: list[set[int]]) -> dict:
+    """Find the most representative comment in a cluster by identifying the
+    most central node
+
+    Args:
+        G (nx.Graph): network graph with comments as nodes and their
+        similarities as weights
+        clusters (list[set[int]]): clusters from Louvain Communities
+
+    Returns:
+        dict: dictionary with the central comment id as the key and the degree
+        centrality as the value
+    """
     centrality_per_cluster = {}
     for cluster in clusters:
+        # focus on each specific cluster of comments
         subgraph = G.subgraph(cluster)
-        # Using degree centrality for simplicity; you can change this metric
+        # calculate the centrality of each comment in the cluster
         centralities = nx.degree_centrality(subgraph)
         # Find the node with the highest centrality
         central_node = max(centralities, key=centralities.get)
         centrality_per_cluster[central_node] = centralities[central_node]
+
     return centrality_per_cluster
 
 
-def representative_comments(G, clusters, df):
+def representative_comments(
+    G: nx.Graph, clusters: list[set[int]], df: pl.DataFrame
+) -> pl.DataFrame:
+    """Creates a dataframe with the text of the representative comments along
+    with the number of comments that are semantically represented by that text
+
+    Args:
+        G (nx.Graph): network graph with comments as nodes and their
+        similarities as weights
+        clusters (list[set[int]]): clusters from Louvain Communities
+        df (pl.DataFrame): df from initial pull with added cluster info
+
+    Returns:
+        output_df (pl.DataFrame): df with representation information
+    """
     central_nodes = find_central_node(G, clusters)
     representative_dict = {
         "comments_represented": [],
@@ -141,9 +222,14 @@ if __name__ == "__main__":
     df = pull_data(
         database, database_user, database_password, database_host, database_port
     )
+
+    # get semantic similarities of comments
     df_paraphrases = comment_similarity(df)
+    # build graph to represent significant similarity relationships
     G = build_graph(df_paraphrases)
+    # cluster top relationships to add another filter for relationship quality
     clusters = get_clusters(G=G)
     # includes text, cluster, comment id for jack
     df = assign_clusters(df=df, clusters=clusters)
+    # print out current format of output
     print(representative_comments(G, clusters, df))
