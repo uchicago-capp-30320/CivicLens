9284857fdc45d035bee6d24c004fcf03bd7948d3
andrewjtdunn <35241751+andrewjtdunn@users.noreply.github.com>
2024-04-30 00:03:09 -0500

    replace api key with DEMO_KEY -- will need to be mocked next

--- civiclens/tests/test_access_api_data.py
+++ civiclens/tests/test_access_api_data.py
@@ -1,5 +1,6 @@
 from civiclens.data_engineering import access_api_data
-from civiclens.utils.constants import REG_GOV_API_KEY
+
+api_key = "DEMO_KEY"
 
 
 def test_is_duplicated_on_server():
@@ -12,7 +13,7 @@ def test_is_duplicated_on_server():
 def test_is_duplicated_on_server_real_data():
     # NRCS-2009-0004-0003 is known duplicated comment ID
     assert access_api_data.pull_reg_gov_data(
-        REG_GOV_API_KEY,
+        api_key,
         "comments",
         params={"filter[searchTerm]": "NRCS-2009-0004-0003"},
     )
@@ -41,7 +42,7 @@ def test_pull_reg_gov_data():
     # https://www.regulations.gov/document/CRC-2024-0073-0001
 
     access_api_data.pull_reg_gov_data(
-        REG_GOV_API_KEY,
+        api_key,
         "documents",
         params={"filter[searchTerm]": "CRC-2024-0073-0001"},
     )
