0070c1ed45d8c2f3095462129e31aef3590c759d
Jack Gibson <jpgibson@uchicago.edu>
2024-05-02 17:36:17 -0500

    extract formletter

--- civiclens/nlp/topics.py
+++ civiclens/nlp/topics.py
@@ -21,6 +21,19 @@ def clean_comments(text: str) -> str:
 
     return text
 
+def truncate(text: str, num_words: int) -> str:
+    """
+    Truncates commments:
+
+    Inputs:
+        text (str): Text of the comment
+        num_words (int): Number of words to keep
+
+    Returns:
+        Truncated commented
+    """
+    words = text.split(' ')
+    return " ".join(words[:num_words])
 
 def build_embeds(words: list[str]) -> dict[str, torch.tensor]:
     """
@@ -83,9 +96,42 @@ def maximal_marginal_relevance(
 
 
 def extract_formletters(
-    df: pl.DataFrame, embeddings: torch.tensor, sim_threhold: float
-):
-    pass
+    docs: list[str], embeddings: torch.tensor, sim_threhold: float = 0.99,
+) -> dict[str, int]:
+    """
+    Extracts from letters from collection of comments.
+
+    Inputs:
+        docs: List of comments
+        embeddings: Embedding matrix for list of comments
+        sim_threhold: Threhold (cosine similarity) for similar comments
+
+    Returns:
+        Dictionary containing text of identified form letters, and number
+        of form comments. 
+    """
+    df = pl.DataFrame({"comment" : docs})
+    df = df.with_row_index()
+
+    form_comments = {}
+
+    for _ in range(len(docs) // 100):
+        row = df.sample(n=1)
+        idx = row['index']
+        sim_tensor = cos_sim(embeddings[idx], embeddings)
+
+        _, indices = torch.where(sim_tensor > sim_threhold)
+        sample = df.filter(~df['index'].is_in(indices.tolist()))
+
+        if (len(df) - len(sample)) / len(df) > 0.25:
+            form_comments[row['comment'].item()] = len(df) - len(sample) 
+        
+        df = sample 
+
+        if df.is_empty():
+            break
+
+    return form_comments
 
 
 class TopicModel:
