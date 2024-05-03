498ea37b7f823f0dcc972a6c2db4dc4531554675
jchristensonCHI <114839369+jchristensonCHI@users.noreply.github.com>
2024-05-03 05:08:03 -0500

    Setup a weighted search

--- civiclens/regulations/views.py
+++ civiclens/regulations/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect  # noqa: F401
 from .models import Comment, Document # noqa: F401
 from .forms import Search
 from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity, SearchHeadline # noqa: F401
+from django.db.models import Value # noqa: F401
 from datetime import datetime # noqa: F401
 from django.core.cache import cache # noqa: F401
 
@@ -22,35 +23,56 @@ def search_page(request):
 def search_results(request):
     
     context = {}
-    ############################################################
-    # get response and save query term to output context
     if request.method == "GET":
-        search = Search()
-        search.search_term = request.GET["q"]
-        # search.sort_by = request.GET["sort_by"]
+        query = request.GET.get("q", "")
+        sort_by = request.GET.get("sort_by", "most_relevant")
         
-        context["Search"] = search
-    
-    print(context)
-    ############################################################
-    if request.method == "GET":
-        content = None
-        query = request.GET.get("q")
         if query:
-            search = Search()
-            search.searchterm = query
-        
-            vector = SearchVector('title')
-            documents = Document.objects.annotate(search=vector).filter(search=query)
+            vector = SearchVector("title", weight="A") + SearchVector(
+                "summary", weight="B") + SearchVector("agency_id", weight="D"
+                ) + SearchVector("agency_type", weight="D") + SearchVector(
+                "further_information", weight="D")
+            search_query = SearchQuery(query)
+            search_headline = SearchHeadline("title", search_query)
+            documents = Document.objects.annotate(rank=SearchRank(vector, 
+                    search_query)).annotate(headline=search_headline).filter(
+                    rank__gte=0.0001)# .order_by("-rank")
+    
+            
+            print(Search.sort_by)
+            print("Sort By:", sort_by)
+            # if sort_by == "most_recent":
+            #     documents = documents.order_by("-posted_date")  # Assuming 'created_at' is a field in your Document model
+            # elif sort_by == "most_comments":
+            #     documents = documents.annotate(comment_count=Count('comments')).order_by("-comment_count")
+            # elif sort_by == "least_comments":
+            #     documents = documents.annotate(comment_count=Count('comments')).order_by("comment_count")
+            # else:  # Default sort by rank if 'most_relevant' or any other unspecified value
+            #     documents = documents.order_by("-rank")
+            
+            documents = documents.order_by("-rank")
+                
+            # if not documents.exists():
+            #     documents = Document.objects.annotate(
+            #         rank=TrigramSimilarity("title", query) +
+            #              TrigramSimilarity("summary", query) +
+            #              TrigramSimilarity("agency_id", query) +
+            #              TrigramSimilarity("agency_type", query) +
+            #              TrigramSimilarity("further_information", query)
+            #     ).filter(rank__gt=0.1).order_by("-rank")
 
             # cache_key = f"search_results_{search.searchterm}"
             # cache.set(cache_key, {'results': results, 'count': len(results)}, timeout=3600)
 
             context['documents'] = documents
-            context['Search'] = search
-
-            print("Search Term:", search.searchterm)
+            print("Search Term:", query)
             print("Documents Found:", documents.count())
+    else:
+        context["documents"] = None
+    
+
+    context["search"] = query
+        
 
     return render(request, "search_results.html", {"context": context})
 
