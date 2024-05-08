df484e40e293be1a5ec0b7cda0e5ae61df87b020
Jack Gibson <jpgibson@uchicago.edu>
2024-05-08 11:32:31 -0500

    add tests

--- civiclens/tests/test_utils.py
+++ civiclens/tests/test_utils.py
@@ -1,22 +1,28 @@
-# from civiclens.utils.database_access import pull_data
-# import polars as pl
-# import pytest
+import polars as pl
+import pytest
 
-# test_query = "SELECT 1"
-# test_schema = ["numbers"]
+from civiclens.utils.database_access import pull_data
 
-# def test_to_df():
-#     out = pull_data(test_query, test_schema)
-#     assert isinstance(out, pl.DataFrame)
 
-# def test_to_list():
-#     out = pull_data(test_query, return_type="list")
-#     assert isinstance(out, list)
+test_query = "SELECT 1"
+test_schema = ["numbers"]
 
-# def test_missing_schema():
-#     with pytest.raises(ValueError):
-#         pull_data(test_query)
 
-# def test_bad_query():
-#     with pytest.raises(RuntimeError):
-#         pull_data("SELECT data FROM not_a_table", return_type="list")
+def test_to_df():
+    out = pull_data(test_query, test_schema)
+    assert isinstance(out, pl.DataFrame)
+
+
+def test_to_list():
+    out = pull_data(test_query, return_type="list")
+    assert isinstance(out, list)
+
+
+def test_missing_schema():
+    with pytest.raises(ValueError):
+        pull_data(test_query)
+
+
+def test_bad_query():
+    with pytest.raises(RuntimeError):
+        pull_data("SELECT data FROM not_a_table", return_type="list")
