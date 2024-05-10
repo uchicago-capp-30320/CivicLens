548a5e2cb69a07b99c98ba03bca59e04f75a2596
Jack Gibson <jpgibson@uchicago.edu>
2024-05-10 09:29:19 -0500

    add test data

--- civiclens/tests/test_utils.py
+++ civiclens/tests/test_utils.py
@@ -1,3 +1,4 @@
+import json
 import sqlite3
 
 import polars as pl
@@ -6,8 +7,12 @@ import pytest
 from civiclens.utils.database_access import pull_data
 
 
+with open("nlp_test_data/mock-row.json", "r") as f:
+    mock_data = json.loads(f)
+
+
 def test_sqlite():
-    query = "SELECT 1"
+    query = "SELECT 1 FROM regulations_nlpoutput"
     schema = ["numbers"]
     conn = sqlite3.connect("test.db")
     out = pull_data(conn, query, schema, return_type="list")
@@ -15,7 +20,7 @@ def test_sqlite():
 
 
 def test_to_df():
-    query = "SELECT 1"
+    query = "SELECT 1 FROM regulations_nlpoutput"
     schema = ["numbers"]
     conn = sqlite3.connect("test.db")
     out = pull_data(conn, query, schema)
@@ -23,20 +28,24 @@ def test_to_df():
 
 
 def test_to_list():
-    query = "SELECT 1"
+    query = "SELECT 1 FROM regulations_nlpoutput"
     conn = sqlite3.connect("test.db")
     out = pull_data(conn, query, return_type="list")
     assert out == [(1,)]
 
 
 def test_missing_schema():
-    query = "SELECT 1"
-    conn = sqlite3.connect("tutorial.db")
+    query = "SELECT 1 FROM regulations_nlpoutput"
+    conn = sqlite3.connect("test.db")
     with pytest.raises(ValueError):
         pull_data(conn, query)
 
 
 def test_bad_query():
-    conn = sqlite3.connect("tutorial.db")
+    conn = sqlite3.connect("test.db")
     with pytest.raises(RuntimeError):
         pull_data(conn, "SELECT data FROM not_a_table", return_type="list")
+
+
+def test_nlp_upload():
+    conn = sqlite3.connect("test.db")
