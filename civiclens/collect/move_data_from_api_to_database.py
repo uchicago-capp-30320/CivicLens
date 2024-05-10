f3ecbf722034c2ff35d2da796f034c59a6232ee7
andrewjtdunn <35241751+andrewjtdunn@users.noreply.github.com>
2024-05-10 09:54:11 -0500

    clean up module paths

--- civiclens/collect/move_data_from_api_to_database.py
+++ civiclens/collect/move_data_from_api_to_database.py
@@ -7,7 +7,14 @@ import datetime as dt
 from datetime import datetime
 
 from civiclens.collect.access_api_data import pull_reg_gov_data
-from civiclens.utils import constants
+from civiclens.utils.constants import (
+    REG_GOV_API_KEY,
+    DATABASE_NAME,
+    DATABASE_USER,
+    DATABASE_PASSWORD,
+    DATABASE_HOST,
+    DATABASE_PORT,
+)
 
 
 def fetch_fr_document_details(fr_doc_num: str) -> str:
@@ -138,11 +145,11 @@ def connect_db_and_get_cursor() -> (
     Connect to the CivicLens database and return the objects
     """
     connection = psycopg2.connect(
-        database=constants.DATABASE_NAME,
-        user=constants.DATABASE_USER,
-        password=constants.DATABASE_PASSWORD,
-        host=constants.DATABASE_HOST,
-        port=constants.DATABASE_PORT,
+        database=DATABASE_NAME,
+        user=DATABASE_USER,
+        password=DATABASE_PASSWORD,
+        host=DATABASE_HOST,
+        port=DATABASE_PORT,
     )
     cursor = connection.cursor()
     return connection, cursor
@@ -295,7 +302,7 @@ def add_dockets_to_db(doc_list: list[dict], print_statements: bool = True) -> No
             not verify_database_existence("regulations_docket", docket_id)
         ):
             docket_data = pull_reg_gov_data(
-                constants.REG_GOV_API_KEY,
+                REG_GOV_API_KEY,
                 "dockets",
                 params={"filter[searchTerm]": docket_id},
             )
@@ -693,15 +700,13 @@ def add_comments_to_db_for_new_doc(document_object_id: str) -> None:
     Returns: nothing; adds comments, if available, to the db
     """
     comment_data = pull_reg_gov_data(
-        constants.REG_GOV_API_KEY,
+        REG_GOV_API_KEY,
         "comments",
         params={"filter[commentOnId]": document_object_id},
     )
     # add comment data to comments table in the database
     for comment in comment_data:
-        all_comment_data = merge_comment_text_and_data(
-            constants.REG_GOV_API_KEY, comment
-        )
+        all_comment_data = merge_comment_text_and_data(REG_GOV_API_KEY, comment)
         insert_response = insert_comment_into_db(all_comment_data)
 
         if insert_response["error"]:
@@ -734,7 +739,7 @@ def add_comments_to_db_for_existing_doc(
         )
 
     comment_data = pull_reg_gov_data(
-        constants.REG_GOV_API_KEY,
+        REG_GOV_API_KEY,
         "comments",
         params={
             "filter[commentOnId]": document_object_id,
@@ -743,9 +748,7 @@ def add_comments_to_db_for_existing_doc(
     )
 
     for comment in comment_data:
-        all_comment_data = merge_comment_text_and_data(
-            constants.REG_GOV_API_KEY, comment
-        )
+        all_comment_data = merge_comment_text_and_data(REG_GOV_API_KEY, comment)
         insert_response = insert_comment_into_db(all_comment_data)
         if insert_response["error"]:
             print(insert_response["description"])
@@ -805,7 +808,7 @@ def pull_all_api_data_for_date_range(
     # get documents
     print("getting list of documents within date range")
     doc_list = pull_reg_gov_data(
-        constants.REG_GOV_API_KEY,
+        REG_GOV_API_KEY,
         "documents",
         start_date=start_date,
         end_date=end_date,
