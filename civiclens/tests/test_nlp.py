4ada43118187c42bc679f5bcf21bb6b614fc4ca9
Abe Burton <77302496+abejburton@users.noreply.github.com>
2024-05-10 08:51:38 -0500

    Abe nlp - rough draft of pipeline (#119)
    * explorratory notebook for representative comments
    
    * create structure for nlp code
    
    * update nlp with docstrings and type hinting
    
    * create data pull util and start data preprocessing for form letters
    
    * text processing, refactor for sets of comments and handling form letters
    
    * representative comment tests
    
    * start migration from notebook
    
    * extract formletter
    
    * tweaks
    
    * initial titles code
    
    * topic model class
    
    * moved to topics branch
    
    * comment and title refactor for pipeline
    
    * rough  draft pipeline
    
    * nlp field additions
    
    * refactor with comment dataclass and refine pipeline
    
    * adjust cluster test
    
    * fix loop
    
    * fix test error
    
    * simplify query for initial doc identification
    
    * refactor for PR feedback
    
    * fix comment similarity test so it uses model param
    
    * fix comment similarity test bug
    
    ---------
    
    Co-authored-by: Abe Burton <abeburton@Abes-MacBook-Pro.local>
    Co-authored-by: Jack Gibson <jpgibson@uchicago.edu>
    Co-authored-by: Abe Burton <abeburton@Abes-MBP.attlocal.net>

---
+++ civiclens/tests/test_nlp.py
@@ -0,0 +1,51 @@
+import polars as pl
+from sentence_transformers import SentenceTransformer
+
+from civiclens.nlp import comments
+
+
+def test_comment_similarity():
+    df = pl.read_csv(
+        "civiclens/tests/nlp_test_data/sample_comments.csv", separator=","
+    )
+    model = SentenceTransformer("all-mpnet-base-v2")
+    df_paraphrase, df_form_letter = comments.comment_similarity(df, model)
+
+    assert df_paraphrase.shape == (377, 4)
+    assert df_form_letter.shape == (1, 4)
+    assert df_paraphrase.columns == df_paraphrase.columns
+    assert df_form_letter.columns == df_form_letter.columns
+
+
+def test_graph():
+    df_paraphrase = pl.read_csv(
+        "civiclens/tests/nlp_test_data/df_paraphrase.csv", separator=","
+    )
+
+    # build graph
+    G = comments.build_graph(df_paraphrase)
+
+    # cluster graph
+    clusters = comments.get_clusters(G=G)
+    assert isinstance(clusters, list), "Clusters is not a list"
+    for cluster in clusters:
+        assert isinstance(cluster, set), "Cluster is not a set"
+
+
+def test_cluster_assignment():
+    # read in correct df with clustered comments
+    df_cluster = pl.read_csv(
+        "civiclens/tests/nlp_test_data/sample_comments_cluster.csv",
+        separator=",",
+    )
+
+    # remove cluster assignments
+    df = df_cluster.with_columns(pl.lit(None).alias("cluster"))
+    clusters = [
+        {0, 1, 2, 4, 6, 7, 8, 9, 11, 12, 13, 17, 18, 23},
+        {3, 5, 10, 14, 15, 16, 19, 20, 21, 22, 24, 25, 26, 27},
+    ]
+
+    # assign clusters and confirm the algorithm is consistent
+    df = comments.assign_clusters(df=df, clusters=clusters)
+    assert df_cluster.equals(df)
