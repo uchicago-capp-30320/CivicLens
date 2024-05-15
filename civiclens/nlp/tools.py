5c69de9eef77c5f445e1caad7d06ca21d6645262
Jack Gibson <jpgibson@uchicago.edu>
2024-05-15 12:20:03 -0500

    removed sentiment erroring

--- civiclens/nlp/tools.py
+++ civiclens/nlp/tools.py
@@ -143,10 +143,6 @@ def sentiment_analysis(comment: Comment, pipeline: pipeline) -> str:
     Returns:
         Sentiment label as string (e.g 'postive', 'negative', 'neutral')
     """
-    try:
-        out = pipeline(comment.text)[0]
-    except Exception as e:
-        print(e)
-        return ""
 
+    out = pipeline(comment.text)[0]
     return out["label"]
