f3ca9c33f292ca4ec2e4700f0ce6c3041fee1d9a
GregoryHo88 <114549980+GregoryHo88@users.noreply.github.com>
2024-05-15 18:45:17 +0000

    Changed test following a slight ammendment to api_date_format_params()

--- civiclens/tests/test_access_api_data.py
+++ civiclens/tests/test_access_api_data.py
@@ -13,28 +13,28 @@ def test_is_duplicated_on_server():
     assert access_api_data._is_duplicated_on_server(test_json)
 
 
-def test_api_date_format_params_dockets():
+def test_api_date_format_params():
     """
-    Ensure we return the right parameters for dockets
+    Ensure we return the right parameters for formatting lastModifiedDate
     """
     assert access_api_data.api_date_format_params(
-        "dockets", "01-01-0001", "12-12-1212"
+        "01-01-0001", "12-12-1212"
     ) == {
         "filter[lastModifiedDate][ge]": "01-01-0001 00:00:00",
         "filter[lastModifiedDate][le]": "12-12-1212 23:59:59",
     }
 
 
-def test_api_date_format_params_NOT_dockets():
-    """
-    Ensure we return the right parameters for documents
-    """
-    assert access_api_data.api_date_format_params(
-        "documents", "01-01-0001", "12-12-1212"
-    ) == {
-        "filter[postedDate][ge]": "01-01-0001",
-        "filter[postedDate][le]": "12-12-1212",
-    }
+# def test_api_date_format_params_NOT_dockets():
+#     """
+#     Ensure we return the right parameters for documents
+#     """
+#     assert access_api_data.api_date_format_params(
+#         "documents", "01-01-0001", "12-12-1212"
+#     ) == {
+#         "filter[postedDate][ge]": "01-01-0001",
+#         "filter[postedDate][le]": "12-12-1212",
+#     }
 
 
 # def test_pull_reg_gov_data():
