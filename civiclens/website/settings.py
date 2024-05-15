8e4b1648774da69c41cddbf3cf16fc8e6bd5077c
John Christenson <114839369+jchristensonCHI@users.noreply.github.com>
2024-05-15 16:22:16 -0500

    Update settings.py

--- civiclens/website/settings.py
+++ civiclens/website/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['civic-lens.org']
+ALLOWED_HOSTS = ["civic-lens.org", "127.0.0.1", "localhost"]
 
 
 # Application definition
