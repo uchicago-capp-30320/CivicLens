706923cd89dae15de763c3a0c2e359a95ebe1c34
jchristensonCHI <114839369+jchristensonCHI@users.noreply.github.com>
2024-05-16 00:52:21 -0500

    linters

--- civiclens/regulations/views.py
+++ civiclens/regulations/views.py
@@ -5,18 +5,13 @@ from django.contrib.postgres.search import (
     SearchVector,
     TrigramSimilarity,
 )
-from django.shortcuts import render
-
-from .models import Comment, Document, AgencyReference
-
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Count
-
 from django.db.models.functions import TruncDate
-
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
-from django.core.exceptions import ObjectDoesNotExist
-
+from .models import AgencyReference, Comment, Document
 
 
 def home(request):
@@ -26,16 +21,16 @@ def home(request):
 def search_page(request):
     return render(request, "search_page.html")
 
-#require method decorqator to only allow GET requests
-def search_results(request):
+
+# require method decorqator to only allow GET requests
+def search_results(request):  # noqa: C901
     today = timezone.now().date()
     context = {}
-    
-    
+
     if request.method == "GET":
         query = request.GET.get("q", "")
         sort_by = request.GET.get("sort_by", "most_relevant")
-        selected_agencies = request.GET.getlist("selected_agencies", "") 
+        selected_agencies = request.GET.getlist("selected_agencies", "")
         search_results = request.GET.get("source", False)
         if search_results:
             comments_any = request.GET.get("comments_any")
@@ -44,8 +39,13 @@ def search_results(request):
             category_proprosed_rule = request.GET.get("proposed_rule")
             category_notice = request.GET.get("notice")
             category_other = request.GET.get("other")
-            category_lst = [category_rule, category_proprosed_rule, category_notice, category_other]
-            
+            category_lst = [
+                category_rule,
+                category_proprosed_rule,
+                category_notice,
+                category_other,
+            ]
+
         if query:
             vector = (
                 SearchVector("title", weight="A")
@@ -59,8 +59,7 @@ def search_results(request):
             documents = (
                 Document.objects.annotate(rank=SearchRank(vector, search_query))
                 .annotate(headline=search_headline)
-                .annotate(comment_count=Count('comment'))
-                .annotate(comment_count=Count('comment'))
+                .annotate(comment_count=Count("comment"))
                 .filter(rank__gte=0.0001)
                 .filter(comment_end_date__gte=today)
                 .order_by("-rank")
@@ -73,133 +72,159 @@ def search_results(request):
                         + TrigramSimilarity("summary", query)
                         + TrigramSimilarity("agency_id", query)
                         + TrigramSimilarity("agency_type", query)
-                        + TrigramSimilarity("agency_type", query)
                     )
-                    .annotate(comment_count=Count('comment'))
-                    .annotate(comment_count=Count('comment'))
+                    .annotate(comment_count=Count("comment"))
                     .filter(rank__gt=0.20)
                     .filter(comment_end_date__gte=today)
-                    .filter(comment_end_date__gte=today)
                     .order_by("-rank")
-                ) 
-            if sort_by == 'most_recent':
+                )
+            if sort_by == "most_recent":
                 documents = documents.order_by("-posted_date")
-            elif sort_by == 'most_comments':
+            elif sort_by == "most_comments":
                 documents = documents.order_by("-comment_count")
-            elif sort_by == 'least_comments':
+            elif sort_by == "least_comments":
                 documents = documents.order_by("comment_count")
-            
+
             if selected_agencies:
                 documents = documents.filter(agency_id__in=selected_agencies)
-            
+
             if search_results:
                 documents = documents.filter(document_type__in=category_lst)
                 if comments_any:
-                    documents = documents.filter(comment_count__gte=1) 
+                    documents = documents.filter(comment_count__gte=1)
                 if comments_over_hundred:
                     documents = documents.filter(comment_count__gte=100)
-            if sort_by == 'most_recent':
+            if sort_by == "most_recent":
                 documents = documents.order_by("-posted_date")
-            elif sort_by == 'most_comments':
+            elif sort_by == "most_comments":
                 documents = documents.order_by("-comment_count")
-            elif sort_by == 'least_comments':
+            elif sort_by == "least_comments":
                 documents = documents.order_by("comment_count")
-            
+
             if selected_agencies:
                 documents = documents.filter(agency_id__in=selected_agencies)
-            
+
             if search_results:
                 documents = documents.filter(document_type__in=category_lst)
                 if comments_any:
-                    documents = documents.filter(comment_count__gte=1) 
+                    documents = documents.filter(comment_count__gte=1)
                 if comments_over_hundred:
                     documents = documents.filter(comment_count__gte=100)
 
             context["documents"] = documents
     else:
         context["documents"] = None
-    
+
     context["search"] = query
 
-    return render(request, "search_results.html", 
-            {"context": context, "agencies": AgencyReference.objects.all().order_by("id")}
+    return render(
+        request,
+        "search_results.html",
+        {
+            "context": context,
+            "agencies": AgencyReference.objects.all().order_by("id"),
+        },
     )
 
 
-def document(request, doc_id):
+def document(request, doc_id):  # noqa: E501
+    today = timezone.now().date()
+
+    doc = get_object_or_404(
+        Document.objects.filter(id=doc_id)
+        .filter(comment_end_date__gte=today)
+        .annotate(comment_count=Count("comment"))
+    )
+
     try:
-        doc = Document.objects.filter(id=doc_id).annotate(comment_count=Count(
-            "comment")
-        ).get()
-        
-    except Document.DoesNotExist:
-        doc = None
-    try:
-        comments_api = Comment.objects.filter(document=doc_id).annotate(
-            posted_date_only=TruncDate("posted_date")).annotate(
-            modify_date_only=TruncDate("modify_date")).annotate(
-            receive_date_only=TruncDate("receive_date")
+        comments_api = (
+            Comment.objects.filter(document=doc_id)
+            .annotate(posted_date_only=TruncDate("posted_date"))
+            .annotate(modify_date_only=TruncDate("modify_date"))
+            .annotate(receive_date_only=TruncDate("receive_date"))
         )
-        comments_last_updated = comments_api.latest("modify_date_only").modify_date_only
+        comments_last_updated = comments_api.latest(
+            "modify_date_only"
+        ).modify_date_only
         unique_comments = comments_api.distinct("comment").count()
     except Comment.DoesNotExist:
         comments_api = None
         unique_comments = 0
-    
+
     if comments_api:
         try:
-            latest_comment = comments_api.latest('modify_date_only')
+            latest_comment = comments_api.latest("modify_date_only")
             comments_last_updated = latest_comment.modify_date_only
         except ObjectDoesNotExist:
             comments_last_updated = "No comments found."
     else:
         comments_last_updated = "No comments found."
-        
+
     # test data from jack
     comments_nlp = {
-        "id": "7588edfc-4239-4970-970e-d080eecf4da7", 
+        "id": "7588edfc-4239-4970-970e-d080eecf4da7",
         "rep_comments": [
-            {"id": "ED-2023-OPE-0123-28272", 
-             "text": "The more student loan debt that can be forgiven the better. Over the years , I have had yo pause my student l9ans because of financial hardships I was facing.The period of time that loans were in repayment I had made my payments on time.My loans are currently in repayment, and if that burden could be lifted it would be life-changing for me. Right now. I find it very difficult to pay off my student loan debt. It has been following me for quite some time. Loan forgiveness would be good if I can qualify for it. ", 
-             "num_represented": 450, 
-             "topic": "Debt Forgiveness", 
-             "form_letter": True}, 
-             {"id": "ED-2023-OPE-0123-28250", 
-              "text": "Hello I am a current student who would greatly appreciate the privilege of having my student loans forgiven. Thank you so much in advance!", 
-              "num_represented": 231, 
-              "topic": "Student Loans", 
-              "form_letter": False}], 
-        "doc_plain_english_title": "Student Loan Debt Waiver: Department Of Education", 
-        "num_total_comments": 980, 
-        "num_unique_comments": 762, 
-        "num_rep_comments": 2, 
-        "topics": 
-            [{"topic": "Debt Forgiveness", 
-              "positive": 129, 
-              "negative": 98, 
-              "neutral": 32}, 
-            {"topic": "Student Loans", 
-             "positive": 123, 
-             "negative": 32, 
-             "neutral": 149
-             },
-            {"topic": "topic 3",
-             "positive": 103, 
-             "negative": 32, 
-             "neutral": 149
-             },
-            {"topic": "topic 4",
-             "positive": 903, 
-             "negative": 32, 
-             "neutral": 149
-             },
-             ],
-        "num_topics": 2, 
+            {
+                "id": "ED-2023-OPE-0123-28272",
+                "text": "The more student loan debt that can be forgiven the better. Over the years , I have had yo pause my student l9ans because of financial hardships I was facing.The period of time that loans were in repayment I had made my payments on time.My loans are currently in repayment, and if that burden could be lifted it would be life-changing for me. Right now. I find it very difficult to pay off my student loan debt. It has been following me for quite some time. Loan forgiveness would be good if I can qualify for it. ",  # noqa: E501
+                "num_represented": 450,
+                "topic": "Debt Forgiveness",
+                "form_letter": True,
+            },
+            {
+                "id": "ED-2023-OPE-0123-28250",
+                "text": "Hello I am a current student who would greatly appreciate the privilege of having my student loans forgiven. Thank you so much in advance!",  # noqa: E501
+                "num_represented": 231,
+                "topic": "Student Loans",
+                "form_letter": False,
+            },
+        ],
+        "doc_plain_english_title": "Student Loan Debt Waiver: Department Of Education",  # noqa: E501
+        "num_total_comments": 980,
+        "num_unique_comments": 762,
+        "num_rep_comments": 2,
+        "topics": [
+            {
+                "topic": "Debt Forgiveness",
+                "positive": 129,
+                "negative": 98,
+                "neutral": 32,
+            },
+            {
+                "topic": "Student Loans",
+                "positive": 123,
+                "negative": 32,
+                "neutral": 149,
+            },
+            {
+                "topic": "topic 3",
+                "positive": 103,
+                "negative": 32,
+                "neutral": 149,
+            },
+            {
+                "topic": "topic 4",
+                "positive": 903,
+                "negative": 32,
+                "neutral": 149,
+            },
+        ],
+        "num_topics": 2,
         "last_updated": "May 6, 2024",
-        "document_id": "ED-2023-OPE-0123-26398"
+        "document_id": "ED-2023-OPE-0123-26398",
     }
-    # comments_nlp = {} 
-    return render(request, "document.html", {"doc": doc, "comments_nlp": comments_nlp, "comments_api": comments_api, "unique_comments": unique_comments, "comments_last_updated": comments_last_updated})
+    # comments_nlp = {}
+    return render(
+        request,
+        "document.html",
+        {
+            "doc": doc,
+            "comments_nlp": comments_nlp,
+            "comments_api": comments_api,
+            "unique_comments": unique_comments,
+            "comments_last_updated": comments_last_updated,
+        },
+    )
 
 
 def comment(request):
