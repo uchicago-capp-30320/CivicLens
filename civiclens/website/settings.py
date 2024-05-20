8f11eacc8ab2c4cbd871ac667fa39bd478cd9fe8
John Christenson <114839369+jchristensonCHI@users.noreply.github.com>
2024-05-20 11:39:11 -0500

    Instituted keeping the previous search and filters/sorts with forms.py & adding logging to views.py (#228)

--- civiclens/website/settings.py
+++ civiclens/website/settings.py
@@ -152,3 +152,23 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": os.path.join(BASE_DIR, "debug.log"),
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
