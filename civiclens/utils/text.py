c7daea0329ae58176ea47a6c14055f94244b864a
andrewjtdunn <35241751+andrewjtdunn@users.noreply.github.com>
2024-05-22 10:39:04 -0500

    remove symbols discovered on live site

--- civiclens/utils/text.py
+++ civiclens/utils/text.py
@@ -34,6 +34,10 @@ def clean_text(text: str, patterns: Optional[list[tuple]] = None) -> str:
     text = re.sub(r"<\s*br\s*/>", " ", text)
     text = re.sub(r"[^a-zA-Z0-9.'\"\?\: -]", "", text)
     text = re.sub(r"\w*ndash\w*", "", text)
+    text = re.sub(r"â", "", text)
+    text = re.sub(r"<br\s*/?>", "", text)
+    text = re.sub(r"&#39;", "'", text)  # this replaces html entity with '
+    text = re.sub(r"&rdquo;", '"', text)  # this replaces html entity with "
 
     if patterns:
         for pattern, replacement in patterns:
